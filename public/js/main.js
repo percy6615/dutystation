8eb3df7238aaa417d9dd31405b67eaeb676bed26
percy6615 <percy6615>
2021-09-27 23:58:50 +0800

    2123

--- public/js/main.js
+++ public/js/main.js
@@ -207,6 +207,41 @@ function createfiltermodal() {
     $("#exampleModalScrollable > div").append(content);
 }
 
+var start = moment().subtract(29, 'days');
+var end = moment();
+
+function cb(start, end) {
+    $('#reportrange span').html(start.format('YYYY/MM/DD ') + ' - ' + end.format('YYYY/MM/DD '));
+}
+var reportrange = $("#reportrange").daterangepicker({
+    "alwaysShowCalendars": true,
+    opens: "left",
+    timePicker: true,
+    startDate: start,
+    endDate: end,
+    cancelButtonClasses: "btn-danger",
+    // ranges: {
+    //     "今天": [moment(), moment()],
+    //     "過去 7 天": [moment().subtract(6, "days"), moment()],
+    //     "本月": [moment().startOf("month"), moment().endOf("month")],
+    //     "上個月": [moment().subtract(1, "month").startOf("month"), moment().subtract(1, "month").endOf("month")]
+    // },
+    locale: {
+        format: "YYYY-MM-DD hh:mm A",
+        separator: " ~ ",
+        applyLabel: "確定",
+        cancelLabel: "清除",
+        fromLabel: "開始日期",
+        toLabel: "結束日期",
+        customRangeLabel: "自訂日期區間",
+        daysOfWeek: ["日", "一", "二", "三", "四", "五", "六"],
+        monthNames: ["1月", "2月", "3月", "4月", "5月", "6月",
+            "7月", "8月", "9月", "10月", "11月", "12月"
+        ],
+        firstDay: 1
+    }
+}, cb);
+
 function createprojectmodal() {
 
     if ($("#exampleModalScrollable > div").find(".modal-content").length > 0) {
